6168b1d295077b86590a3ad59c9e9203e5ef7bd2
Ivan Lopez <ivan.lopez@credicentro.com.py>
2023-06-11 02:23:07 -0400

    adding "reverseString" solution

--- solutions/Main.java
+++ solutions/Main.java
@@ -11,7 +11,7 @@ public class Main {
         int [] array2 = {1,1,1,2,3,3};
 
         Intersection inter = new Intersection();
-        int [] intersection; // = new int [inter.intersect(array1,array2).length];
+        int [] intersection;
 
         intersection = inter.intersect(array1,array2);
         System.out.println(Arrays.toString(intersection));
@@ -22,6 +22,27 @@ public class Main {
         int target = 25;
         int [] indexOfTwoSums = solution.twoSum(nums, target);
         System.out.println(Arrays.toString(indexOfTwoSums));
+
+        // Reverse a string
+        /*
+           Example 1:
+                             i = init        j = end
+                             0   1   2   3   4
+                Input: s = ["h","e","l","l","o"]     length = 5     stop condition i == j
+                Output:    ["o","l","l","e","h"]
+
+            Example 2:
+                             i  i+1 i+2 j-2 j-1  j
+                             0   1   2   3   4   5   length = 6     stop condition (i > j)
+                Input: s = ["H","a","n","n","a","h"]
+                Output:    ["h","a","n","n","a","H"]
+        */
+        char [] s1 = {'h','e','l','l','o'};
+        char [] s2 = {'H','a','n','n','a', 'h'};
+        solution.reverseString((s1));
+        System.out.println(s1);
+        solution.reverseString((s2));
+        System.out.println(s2);
 
     }
 }
--- solutions/Solution.java
+++ solutions/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
 
         List <Integer> arrayList = new ArrayList<>();
         /*                lenght -2     lenght -1
-          c          0  1    2             3
+                    0  1    2             3
             nums = {8, 1,   9,           18} , target = 27
                    if nums[i] + nums[j] == target
 
@@ -26,5 +26,33 @@ public class Solution {
             }
         }
         return arrayList.stream().mapToInt(k->k).toArray();
+    }
+
+
+    /*
+        Example 1:
+                         i = init        j = end
+                         0   1   2   3   4
+            Input: s = ["h","e","l","l","o"]     length = 5     stop condition i == j
+            Output:    ["o","l","l","e","h"]
+
+        Example 2:
+                         i  i+1 i+2 j-2 j-1  j
+                         0   1   2   3   4   5   length = 6     stop condition (i > j)
+            Input: s = ["H","a","n","n","a","h"]
+            Output:    ["h","a","n","n","a","H"]
+     */
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length-1;
+        char tmp;
+
+        while((i < s.length/2) ) {
+            // swap values
+            tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+            i++; j--;
+        }
     }
 }
